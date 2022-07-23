8c4c18f0a42c13b75bc0c2badfe8892d98cafad3
swittuthcoding <swittuthcoding@gmail.com>
2022-07-23 00:21:08 -0400

    getting meal state to the recipe display

--- client/components/RecipeDisplay.jsx
+++ client/components/RecipeDisplay.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getMeals } from "../redux/reducers/recipeSlice";
 
 export function RecipeDisplay() {
   const dispatch = useDispatch();
+  const mealState = useSelector((state) => state.meals);
 }
